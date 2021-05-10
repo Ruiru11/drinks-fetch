ded501a62be25c16135bf6ddeecdd11042d5418b
Erastus Ruiru <erastusruiru11@gmail.com>
2021-05-10 13:50:34 +0300

    configure firebase

---
+++ src/utils/setUpFirebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+const firebaseConfig = {
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  databaseURL: process.env.REACT_APP_databaseURL,
+  projectId: process.env.REACT_APP_projectId,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const storage = firebase.storage();
+export const db = firebase.firestore();
