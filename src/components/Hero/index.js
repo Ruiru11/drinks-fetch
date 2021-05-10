b7044bae72f9e37adfb7faba4fe0798ae0677664
Erastus Ruiru <erastusruiru11@gmail.com>
2021-05-10 15:12:45 +0300

    remove console.log

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,16 +1,9 @@
-// import { useDispatch, useSelector } from "react-redux";
-// import { useEffect } from "react";
 import { Layout } from "antd";
-import HeaderItem from '../header'
-// import { fetchPopular, fetchRandom, fetchLatest } from '../../redux/actions'
-// import { useHistory } from "react-router-dom";
+import HeaderItem from "../header";
 
-const { Footer } = Layout
+const { Footer } = Layout;
 
 const Hero = () => {
-  //   const dispatch = useDispatch();
-  //   const history = useHistory();
-
   return (
     <Layout className="layout">
       <HeaderItem />
